98ce15dd8a1ce04eee2501f3e4342cba14ddc707
martell <martell@atomsplitgames.com>
2012-11-25 03:32:12 +0000

    iOS build

---
+++ Box2D/build-ios-simulator-xcode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir build
+cd build
+mkdir ios
+cd ios
+
+cmake -G"Xcode" -DCMAKE_TOOLCHAIN_FILE=cmake/ios.cmake -DIOS_PLATFORM="SIMULATOR" ../..
+read -n1 -r -p "Press any key to continue..." key
+xcodebuild -sdk iphoneos -configuration Release -target ALL_BUILD
