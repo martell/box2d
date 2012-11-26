49f1bcf26fd377ebbd4e20a872c30c48d79d9b18
martell <martell@atomsplitgames.com>
2012-11-26 04:31:57 +0000

    final fix for iOS

--- Box2D/build-ios-xcode.sh
+++ Box2D/build-ios-xcode.sh
@@ -7,4 +7,4 @@ cd ios
 
 cmake -G"Xcode" -DCMAKE_TOOLCHAIN_FILE=cmake/ios.cmake -DIOS_PLATFORM="OS" ../..
 read -n1 -r -p "Press any key to continue..." key
-xcodebuild -sdk iphoneos -configuration Release -target ALL_BUILD
+xcodebuild -sdk iphoneos -configuration Debug -target ALL_BUILD
